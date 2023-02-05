0539b683d67771ae8fb332bde5474f5a1ca14323
Snir Yore <yore.snir@gmail.com>
2023-02-05 08:53:32 +0200

    conflict

---
+++ selenium_Dev.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+driver =
+webdriver.Chrome(executable_path="C:/Users/snirm/Desktop/DevOps/chromedriver.
+exe")
+driver.get ("https://www.ynet.co.il")
